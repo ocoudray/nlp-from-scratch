9d4d6ef849e185ff20116549e435e51b4132bb7e
ocoudray <ocdy96@gmail.com>
2025-03-19 22:10:43 +0100

    ENV - add ruff + reformat

--- scripts/prepare_chunks.py
+++ scripts/prepare_chunks.py
@@ -1,12 +1,16 @@
+import json
+
+from tqdm.auto import tqdm
+
 from nlp_from_scratch.constants import CHUNKS_POSITIONS_PATH
 from nlp_from_scratch.dataset import TextDataset
-from nlp_from_scratch.utils import get_chunks_from_text, filter_text
-from tqdm.auto import tqdm
-import json
+from nlp_from_scratch.utils import filter_text, get_chunks_from_text
 
 text_dataset = TextDataset.load_from_save()
 chunks = []
 for k in tqdm(range(len(text_dataset.corpus))):
-    chunks += get_chunks_from_text(filter_text(text_dataset.corpus[k]["text"]), k, text_dataset.tokenizer)
+    chunks += get_chunks_from_text(
+        filter_text(text_dataset.corpus[k]["text"]), k, text_dataset.tokenizer
+    )
 with open(CHUNKS_POSITIONS_PATH, "w") as f:
     json.dump(chunks, f)
