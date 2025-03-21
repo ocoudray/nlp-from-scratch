5f67c6f011e086800f593dee495bf863dc09bd86
ocoudray <ocdy96@gmail.com>
2025-03-21 08:13:52 +0100

    FIX - collecting tests

--- scripts/compute_tokens_frequency.py
+++ scripts/compute_tokens_frequency.py
@@ -7,9 +7,12 @@ from nlp_from_scratch.constants import FREQUENCIES_SAVE_PATH
 from nlp_from_scratch.dataset import TextDataset
 from nlp_from_scratch.utils import convert_counter_to_npy
 
-text_dataset = TextDataset.load_from_save()
-counter = Counter()
-for k in tqdm(range(len(text_dataset))):
-    counter.update(text_dataset.tokenizer.encode(text_dataset.corpus[k]["text"]).ids)
-frequencies = convert_counter_to_npy(counter)
-np.save(FREQUENCIES_SAVE_PATH, frequencies)
+if __name__ == "__main__":
+    text_dataset = TextDataset.load_from_save()
+    counter = Counter()
+    for k in tqdm(range(len(text_dataset))):
+        counter.update(
+            text_dataset.tokenizer.encode(text_dataset.corpus[k]["text"]).ids
+        )
+    frequencies = convert_counter_to_npy(counter)
+    np.save(FREQUENCIES_SAVE_PATH, frequencies)
